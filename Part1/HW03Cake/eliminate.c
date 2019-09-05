3aef94395b1b94faf3097f82be0f156a9315297f
harivanshv <54281173+harivanshv@users.noreply.github.com>
2019-09-04 21:46:15 -0400

    Update eliminate.c

--- Part1/HW03Cake/eliminate.c
+++ Part1/HW03Cake/eliminate.c
@@ -20,18 +20,49 @@ void eliminate(int n, int k)
       fprintf(stderr, "malloc fail\n");
       return;
     }
-  // initialize all elements
+// initialize all elements
+  int i;
+  int count;
+  int testcase;
 
-
-  
   // counting to k,
   // mark the eliminated element
   // print the index of the marked element
   // repeat until only one element is unmarked
 
-
-
-
+  testcase = 0;
+  count = 0;
+ 
+  for (i = 0; i < n; i++)
+  { 
+    count++;
+    
+    if (count == k)
+    { 
+      count = 0; 
+      if (arr[i] == -1)
+      { 
+        count--; 
+        testcase ++;
+      }
+      else
+      { 
+        printf("%d\n", arr[i]);
+        arr[i] = -1;
+        testcase = 0;
+      }
+    }
+    
+    if (i < n)
+    { 
+      i = 0;
+    }
+    
+    if (testcase > n)
+    { 
+      break;
+    }
+  }
   // print the last one
 
 
